0e6f009e8b0605172098308f6bfc6d3b01a91ad6
DaisyAwo <daisyawo27@gmail.com>
2023-04-02 15:41:03 -0700

    Added user model

--- src/db.py
+++ src/db.py
@@ -1,1 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    password = db.Column(db.String)
+    gender = db.Column(db.String)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "password": self.password,
+            "gender" : self.gender
+        }
